2613ec62f0f38b39a66c038c9465c2cddf6a0c69
Adesh  Kumar Dubey <104526444+adesh9199@users.noreply.github.com>
2024-07-16 23:49:46 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# WanderLust
+# wanderlust
---
+++ models/listing.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Define the schema
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    filename: { type: String, default: "listingimage" },
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+      set: (v) =>
+        v === ""
+          ? "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          : v,
+    },
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+// Create the model
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
